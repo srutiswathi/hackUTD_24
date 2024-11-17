1665033ed6e321b4bc10f7bbfedeafd77321d0aa
rujvid <Rujvi.doppalapudi@gmail.com>
2024-11-17 08:25:25 -0600

    working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from home import home_page
 from comparison import comparison_page
 from analysis2 import analysis_page
+from an import create_pygwalker_viz
 
 
 
@@ -14,7 +15,7 @@ st.set_page_config(
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Comparison", "Analysis"])
+page = st.sidebar.radio("Go to", ["Home", "Comparison", "Analysis", "Customizable Data"])
 
 # Page routing
 if page == "Home":
@@ -23,3 +24,5 @@ elif page == "Comparison":
     comparison_page()  # Call the function from comparison.py
 elif page == "Analysis":
     analysis_page()  # Call the function from analysis.py
+elif page == "Customizable Data":
+    create_pygwalker_viz()  # Call the function from analysis.py
